aa1766b5568f61ef20a333d4ca2ffe1fa46da640
WangTing <email@1947475827@qq.com>
2018-11-04 14:23:02 +0800

    完成包括老板、大神、消息、个人中心的四个页面的头部和尾部以及切换
    git commit -m 完成包括老板、大神、消息、个人中心的四个页面的头部和尾部以及切换

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import {Route,Switch} from 'react-router-dom';
 import {NavBar} from 'antd-mobile';
+import Cookies from 'js-cookie';
+
 import LaobanInfo from '../../containers/laobaninfo';
 import DashenInfo from '../../containers/dasheninfo';
 import Dashen from '../../containers/dashen';
@@ -8,7 +10,12 @@ import Laoban from '../../containers/laoban';
 import Message from '../../containers/message';
 import Personal from '../../containers/personal';
 import NavFooter from '../nav-footer';
+import PropTypes from 'prop-types';
 class Main extends Component{
+    static propTypes = {
+        user:PropTypes.object.isRequired,
+        getUserInfo:PropTypes.func.isRequired,
+    }
     //与state相同，都用于保存状态数据
     navList = [
         {
@@ -41,6 +48,22 @@ class Main extends Component{
         }
     ]
     render(){
+
+        //3.本地有cookie,并且redux有值，直接使用
+        const userId = Cookies.get(userid);
+        //1.未登录（用户一上来就访问登录页面），本地没有cookie,跳转到登录
+        if(!userId){
+            //登录
+            this.props.history.replace('/login');
+            return
+        }
+        //2.本地有cookie，登录了刷新了，redux更新没有状态（重置为空），根据cookie重新发送请求，（cookie保存的是user_id,数据库中对用用户的id值）请求当前用户的状态数据，保存在redux中
+        const {_id} = this.props.user;
+        if(!_id){//如果没有id我们就说redu没有更新状态
+            //发送请求，请求用户数据
+            this.props.getUserInfo();
+            return
+        }
         const {navList} = this;
         //获取当前路由路径
         const {pathname} = this.props.location;
@@ -48,9 +71,7 @@ class Main extends Component{
         //遍历数组找到其中的一条对象，这条对象上的路径与浏览器端切换的路径一致
         const currentNav = navList.find(nav =>pathname === nav.path);
 
-        //1.未登录（用户一上来就访问登录页面），本地没有cookie,跳转到登录
-        //2.本地有cookie，登录了刷新了，redux更新没有状态（重置为空），根据cookie重新发送请求，（cookie保存的是user_id,数据库中对用用户的id值）请求当前用户的状态数据，保存在redux中
-        //3.本地有cookie,并且redux有值，直接使用
+
         return (
            <div>
                {currentNav?<NavBar>{currentNav.title}</NavBar>:''}
